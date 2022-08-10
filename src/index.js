83572b7e8b87f1035afbb17c3dc6c87d95e38fdc
Aleazar yilma <yilma4060@gmail.com>
2022-08-10 22:13:24 +0300

    Add some functionality Create/update/Delete and Compelete

--- src/index.js
+++ src/index.js
@@ -1,46 +1,16 @@
 import './style.css';
+import Task from './task.js';
+import storeTask from './taskService.js';
+import manipulateInterface from './updateInterface.js';
 
-const itemsContainer = document.querySelector('.items');
-const taskList = [
-  {
-    index: '1',
-    description: 'Read about webpack',
-    isCompeleted: false,
-  },
-  {
-    index: '2',
-    description: 'Read about java script',
-    isCompeleted: false,
-  },
-  {
-    index: '3',
-    description: 'Read about Linked list',
-    isCompeleted: true,
-  },
-];
-const generateItems = (taskitem) => {
-  let result = '';
-  if (taskitem.isCompeleted) {
-    result = `
-        <div class="item">
-        <input type="checkbox" name="checkTask" class="checktask" checked>
-        <span class="underline">${taskitem.description}</span>
-        <i class="fa fa-ellipsis-v move-btn" aria-hidden="true"></i>
-    </div> 
-        `;
-  } else {
-    result = `
-        <div class="item">
-        <input type="checkbox" name="checkTask" class="checktask" >
-        <span>${taskitem.description}</span>
-        <i class="fa fa-ellipsis-v move-btn" aria-hidden="true"></i>
-    </div> 
-        `;
+const addtaskInput = document.querySelector('.additem');
+
+manipulateInterface.displayTasks();
+
+addtaskInput.addEventListener('keypress', (event) => {
+  if (event.key === 'Enter') {
+    const taskItem = new Task(0, addtaskInput.value, false);
+    storeTask.saveTask(taskItem);
+    manipulateInterface.displayTasks();
   }
-  return result;
-};
-let htmlContent = '';
-taskList.forEach((taksitem) => {
-  htmlContent += generateItems(taksitem);
 });
-itemsContainer.innerHTML = htmlContent;
