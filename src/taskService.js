fb74af18c8e2c604391d29699d560a0a485da606
Aleazar yilma <yilma4060@gmail.com>
2022-08-17 18:34:16 +0300

    create unit test file and jest package

--- src/taskService.js
+++ src/taskService.js
@@ -26,6 +26,7 @@ class storeTask {
     }
     tasks.push(task);
     localStorage.setItem('task', JSON.stringify(tasks));
+    return task;
   }
 
   static removeTask(index) {
@@ -37,6 +38,7 @@ class storeTask {
     });
     tasks = storeTask.reArrange(tasks);
     localStorage.setItem('task', JSON.stringify(tasks));
+    return tasks;
   }
 
   static removeCompeletedTask() {
@@ -44,18 +46,21 @@ class storeTask {
     tasks = tasks.filter((x) => x.isCompeleted === false);
     tasks = storeTask.reArrange(tasks);
     localStorage.setItem('task', JSON.stringify(tasks));
+    return tasks;
   }
 
   static updateStatus(index, status) {
     const tasks = storeTask.getTasks();
     tasks[index - 1].isCompeleted = status;
     localStorage.setItem('task', JSON.stringify(tasks));
+    return tasks;
   }
 
   static updateDescription(index, desc) {
     const tasks = storeTask.getTasks();
     tasks[index - 1].description = desc;
     localStorage.setItem('task', JSON.stringify(tasks));
+    return tasks;
   }
 }
 
